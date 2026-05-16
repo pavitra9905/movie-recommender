795a16208ba2fff6b93221567d4064afcbdbb330
pavitra9905 <pavitrasharma9905@gmail.com>
2026-05-17 00:33:05 +0530

    Remove sensitive and unnecessary files from tracking

--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-# This file stores configuration values like API keys.
-# Keeping them separate from code is a best practice.
-OMDB_API_key = "8ac3a932"
-OMDB_Base_URL = "http://www.omdbapi.com/"
